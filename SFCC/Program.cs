f3025c270d7365af086983116cd39fbbe1ed5304
Lawrence Walters <lwalters@lyonscg.com>
2017-12-14 17:06:45 -0600

    command line flags to parse user specified input files

--- SFCC/Program.cs
+++ SFCC/Program.cs
@@ -22,15 +22,42 @@ namespace SFCC
     {
         static void Main(string[] args)
         {
+            if (Array.IndexOf(args, "-h") > -1 || Array.IndexOf(args, "--help") > -1)
+            {
+                System.Console.WriteLine("Catalog inspector - use this to parse site and master catalog xml and output lists of all primary and classification category assignment for products as well as details for all variation products including their master product, size, color and if they are assigned to a category");
+                return;
+            }
+
+            string masterFile = "master-catalog.xml";
+            string siteFile = "site-catalog.xml";
+
+             if (Array.IndexOf(args, "-m") > -1)
+            {
+                if (args.Length > (Array.IndexOf(args, "-m") + 1))
+                {
+                    masterFile = args[Array.IndexOf(args, "-m") + 1];
+                }
+                else { System.Console.WriteLine("-m argument specified, but no file specified. defaulting to {0}", masterFile); }
+            }
+            if (Array.IndexOf(args, "-s") > -1)
+            {
+                if (args.Length > (Array.IndexOf(args, "-s") + 1))
+                {
+                    siteFile = args[Array.IndexOf(args, "-s") + 1];
+                }
+                else { System.Console.WriteLine("-s argument specified, but no file specified. defaulting to {0}", siteFile); }
+            }
+            System.Console.WriteLine("Reading from master catalog {0} and site catalog {1}", masterFile, siteFile);
+
             // load master catalog
             XmlDocument docMasterCatalog = new XmlDocument();
-            docMasterCatalog.Load("C:\\Users\\lwalters\\docs\\proj\\ash\\meta\\catalog\\master-2017-12-07.xml");
+            docMasterCatalog.Load(masterFile);
             XmlNamespaceManager ns1 = new XmlNamespaceManager(docMasterCatalog.NameTable);
             ns1.AddNamespace("dw", "http://www.demandware.com/xml/impex/catalog/2006-10-31");
 
             // load site catalog
             XmlDocument docSiteCatalog = new XmlDocument();
-            docSiteCatalog.Load("C:\\Users\\lwalters\\docs\\proj\\ash\\meta\\catalog\\site-2017-12-07.xml");
+            docSiteCatalog.Load(siteFile);
             XmlNamespaceManager ns2 = new XmlNamespaceManager(docSiteCatalog.NameTable);
             ns2.AddNamespace("dw", "http://www.demandware.com/xml/impex/catalog/2006-10-31");
 
@@ -61,8 +88,9 @@ namespace SFCC
                 }
                 categoryAssignments.Write(",");
 
-                if(nodeProduct.SelectSingleNode("dw:classification-category",ns1) != null && nodeProduct.SelectSingleNode("dw:classification-category",ns1).InnerText.Length > 0) {
-                    categoryAssignments.Write(nodeProduct.SelectSingleNode("dw:classification-category",ns1).InnerText);
+                if (nodeProduct.SelectSingleNode("dw:classification-category", ns1) != null && nodeProduct.SelectSingleNode("dw:classification-category", ns1).InnerText.Length > 0)
+                {
+                    categoryAssignments.Write(nodeProduct.SelectSingleNode("dw:classification-category", ns1).InnerText);
                 }
                 categoryAssignments.WriteLine();
 

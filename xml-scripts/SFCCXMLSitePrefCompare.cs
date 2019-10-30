78ba007e0cf7c8449f09f0a06fc02a6d75458a1a
Lawrence Walters <lwalters@lyonscg.com>
2019-10-30 22:03:09 +0100

    no newlines in site pref compare
    easier to view out put with no newlines (eg for json prefs).
    
    also renamed to be meaningful, added compiler instructions

---
+++ xml-scripts/SFCCXMLSitePrefCompare.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+/// <summary>
+/// This is used to compare site preference values based on the values configured for different instances. It's useful for making sure all the 
+/// Staging values have been set for Production before a release (or similar for Development). Comment or uncomment the relevant sections based on 
+/// what you want to know.
+/// 
+/// Compiling to executable
+/// ----------------------
+/// Compile this to a SFCCXMLSitePrefCompare.exe with:
+/// PS C:\> csc SFCCXMLSitePrefCompare.cs
+/// https://msdn.microsoft.com/en-us/library/78f4aasd(v=vs.100).aspx
+/// 
+/// Executing
+/// ---------------------
+/// PS C:\> .\SFCCXMLSitePrefCompare.exe [your xml file]
+/// PS C:\> .\SFCCXMLSitePrefCompare.exe preferences.xml
+/// </summary>
+class Program {
+
+    static XNamespace ns = "http://www.demandware.com/xml/impex/preferences/2007-03-31";
+
+    static void Main(string[] args) {
+        string targetInstance = "production";
+        foreach (var item in args) {
+            if (item.StartsWith("-")) {
+                targetInstance = item.Substring(1);
+            }
+        }
+        Console.WriteLine("Pref\tStaging Val\tProduction Val");
+        new List<string> { "custom-preferences", "system-preferences" }.ForEach(type => {
+            var xDocs = from f in args.Where(fname=>!fname.StartsWith("-"))
+            select new {
+                Name = new FileInfo(f).Name
+                , Doc = XDocument.Load(f)
+            };
+            var prefs = xDocs.SelectMany(d => d.Doc.Descendants(ns + type)).ToArray();
+            var ids = prefs.Descendants(ns + "preference").Attributes("preference-id").Select(att => att.Value).Distinct();
+
+            foreach (var id in ids) {
+                var vals = (
+                    from d in xDocs
+                    let customPrefs = d.Doc.Root.Descendants(ns + type).First()
+                    //let devVal = customPrefs.Element(ns + "development")
+                    //                    .Elements(ns + "preference")
+                    //                    .Where(ele => ele.Attribute("preference-id").Value == id)
+                    //                    .Select(ele => ele.Value)
+                    let stagingVal = customPrefs.Element(ns + "staging")
+                                        .Elements(ns + "preference")
+                                        .Where(ele => ele.Attribute("preference-id").Value == id)
+                                        .Select(ele => ele.Value)
+                    let prodVal = customPrefs.Element(ns + "production")
+                                        .Elements(ns + "preference")
+                                        .Where(ele => ele.Attribute("preference-id").Value == id)
+                                        .Select(ele => ele.Value)
+                    //let val = customPrefs.Element(ns + targetInstance)
+                    //                    .Elements(ns + "preference")
+                    //                    .Where(ele => ele.Attribute("preference-id").Value == id)
+                    //                    .Select(ele => ele.Value)
+                    select new {
+                        FileName = d.Name
+                        , Preference = id
+                        , StagingValue = stagingVal.FirstOrDefault()
+                        , ProductionValue = prodVal.FirstOrDefault()
+                    }
+                );
+                //if (vals.Select(v => v.Development).Distinct().Count() != 1) {
+                //    Console.WriteLine();
+                //    Console.WriteLine("Dev Values for {0} differ:", id);
+                //    foreach (var valSet in vals) {
+                //        Console.WriteLine("File {0} has {1}", valSet.FileName, valSet.Development);
+                //    }
+                //}
+                //if (vals.Select(v => v.Staging).Distinct().Count() != 1) {
+                //    Console.WriteLine();
+                //    Console.WriteLine("Staging Values for {0} differ:", id);
+                //    foreach (var valSet in vals) {
+                //        Console.WriteLine("File {0} has {1}", valSet.FileName, valSet.Staging);
+                //    }
+                //}
+                //if (vals.Select(v => v.Value).Distinct().Count() != 1) {
+                //    Console.WriteLine();
+                //    Console.WriteLine("{0} {1} for {2} differ:", targetInstance, type, id);
+                //    foreach (var valSet in vals) {
+                //        Console.WriteLine("{0}:\t{1}", valSet.FileName, valSet.Value);
+                //    }
+                //}
+                if (vals.Where(v=>v.StagingValue != v.ProductionValue).Any()) {
+                    var item = vals.First();
+                    var stgval = "";
+                    var prodval = "";
+                    if(!String.IsNullOrEmpty(item.StagingValue)) {
+                        stgval = item.StagingValue.Replace("\n", " ");
+                        
+                    }
+                    if(!String.IsNullOrEmpty(item.ProductionValue)) {
+                        prodval = item.ProductionValue.Replace("\n"," ");
+                    }
+                    // Console.WriteLine("{0}\t{1}\t{2}",item.Preference, item.StagingValue.Replace(System.Environment.NewLine," "), item.ProductionValue.Replace(System.Environment.NewLine, " "));
+                    Console.WriteLine("{0}\t{1}\t{2}",item.Preference, stgval, prodval);
+                }
+            }
+        });
+        
+    }
+}
--- xslt/catalog-manipulation/get-variants.xslt
+++ xslt/catalog-manipulation/get-variants.xslt
@@ -25,7 +25,7 @@
     </xsl:template>
     <xsl:template match="dw:header" />
     <xsl:template match="dw:product"/>
-    <xsl:template match="dw:product[contains('0BT4191001001|BT4191101002|BT4191105003|ES4502|BT4191101003|BT4191101001|BT4191001003|BT4191001002|BT4191105001|BT419110500',@product-id)]">
+    <xsl:template match="dw:product[contains('CT1000|ES4093|ES4442|ES4502|ES4504|ES4505|ES4506SET|ES4507|ES4573|FS4735|FS5151|FS5507|FS5508|FS5509|FS5510|FS5511|FS5524|FTW1177|FTW1178|FTW1179|FTW1180|FTW6013|FTW6017|FTW6020|FTW6022|FTW6023|FTW6024|FTW6025|FTW6026|FTW6027|FTW6028|JF03108710|JF03109793|JF03110040|ML4059222|S141179|S141181|S141182|S141184|S221428|S221430|S221431|ZB6760001|ZB6760200|ZB7256231|ZB7256981|ZB7314005|ZB7314414|ZB7314998',@product-id)]">
         <xsl:variable name="masterProduct" select="."/>
         <xsl:for-each select="dw:variations/dw:variants/dw:variant">
             <xsl:if test="$catalog/dw:product[@product-id = current()/@product-id]">

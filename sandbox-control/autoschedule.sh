f871a53ff25ae6589af413d96ec2d106b93f9cf4
Lawrence Walters <lawrencewalters@users.noreply.github.com>
2025-08-08 10:16:38 -0400

    set autoschedule flag to true when setting the schedule!

--- sandbox-control/autoschedule.sh
+++ sandbox-control/autoschedule.sh
@@ -10,6 +10,6 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 sfcc-ci client:auth -r
-sfcc-ci sandbox:update --sandbox $1 --start-scheduler "{\"weekdays\":[\"MONDAY\",\"TUESDAY\",\"WEDNESDAY\",\"THURSDAY\",\"FRIDAY\"],\"time\":\"$2:00:00-05:00\"}" --stop-scheduler "{\"weekdays\":[\"MONDAY\",\"TUESDAY\",\"WEDNESDAY\",\"THURSDAY\",\"FRIDAY\"],\"time\":\"$3:00:00-05:00\"}"
+sfcc-ci sandbox:update --sandbox $1 --auto-scheduled true --start-scheduler "{\"weekdays\":[\"MONDAY\",\"TUESDAY\",\"WEDNESDAY\",\"THURSDAY\",\"FRIDAY\"],\"time\":\"$2:00:00-05:00\"}" --stop-scheduler "{\"weekdays\":[\"MONDAY\",\"TUESDAY\",\"WEDNESDAY\",\"THURSDAY\",\"FRIDAY\"],\"time\":\"$3:00:00-05:00\"}"
 # this directly runs our patched sfcc-ci
 # node ../../sfcc-ci/cli.js sandbox:update --sandbox $1 --stop-scheduler '{"weekdays":["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"],"time":"07:00:00-05:00"}' --stop-scheduler '{"weekdays":["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"],"time":"19:00:00-05:00"}'
